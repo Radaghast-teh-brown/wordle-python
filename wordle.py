cebf3d3e4d607f2c42fca24e5df225d022e4a653
Magno Junior <magnobrito00@gmail.com>
2022-10-29 15:46:50 -0300

    Reading the file

--- wordle.py
+++ wordle.py
@@ -1,3 +1,11 @@
 print("-"*40)
 print(15*" " + "WORDLE")
 print("-"*40)
+
+list_of_words = list()
+f = open("words.txt", "r")
+for x in f:
+    list_of_words.append(x.lower().replace("\n",""))
+print(list_of_words)
+
+f.close();
