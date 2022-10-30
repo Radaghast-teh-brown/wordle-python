77c47bf3faba1d405bea7ac87c442a5776ef295e
Magno Junior <magnobrito00@gmail.com>
2022-10-30 15:41:13 -0300

    Typing area

--- wordle.py
+++ wordle.py
@@ -1,11 +1,29 @@
 print("-"*40)
 print(15*" " + "WORDLE")
 print("-"*40)
+print("You have six chances to guess the word.")
+print("-"*40)
+
+
+# Reading the file and storing the words in a list ------------------
 
 list_of_words = list()
 f = open("words.txt", "r")
 for x in f:
     list_of_words.append(x.lower().replace("\n",""))
 print(list_of_words)
-
 f.close();
+
+# Typing area--------------------------------------------------------
+answer = ["-","-","-","-","-"]
+for i in range(6):
+    word = input("This is your {d}° chance: ".format(d = i+1))
+    verify = True
+    while verify:
+        if len(word) != 5 or not(word.isalpha()):
+            word = input("Invalid word. Type again: ")
+        else:
+            verify = False
+
+
+
